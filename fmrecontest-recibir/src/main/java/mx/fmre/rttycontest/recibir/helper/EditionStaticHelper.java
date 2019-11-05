5d48d73022d89002a0d0d1834ed56c91d0c2e92a
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2019-11-05 10:27:02 -0600

    avance

---
+++ fmrecontest-recibir/src/main/java/mx/fmre/rttycontest/recibir/helper/EditionStaticHelper.java
@@ -0,0 +1,25 @@
+package mx.fmre.rttycontest.recibir.helper;
+
+import mx.fmre.rttycontest.persistence.model.Edition;
+import mx.fmre.rttycontest.persistence.model.Email;
+import mx.fmre.rttycontest.recibir.dto.AttachedFileDTO;
+
+public class EditionStaticHelper {
+	private EditionStaticHelper() {
+		// not called
+	}
+	
+	public static String filename(Email email, Edition edition, AttachedFileDTO fileDTO) {
+		return String.format("contest_%d/edition_%d/emailcount_%d/%s", 
+				edition.getContest().getId(), 
+				edition.getId(),
+				email.getEmailCount(), 
+				fileDTO.getFilename());
+	}
+
+	public static String createBucketName(Edition edition) {
+		Integer contestId = edition.getContest().getId();
+		Integer editionId = edition.getId();
+		return "contest_" + contestId.intValue() + "/" + "edition_" + editionId.intValue();
+	}
+}
--- fmrecontest-recibir/src/main/java/mx/fmre/rttycontest/recibir/services/impl/FileManagerServiceAwsS3Impl.java
+++ fmrecontest-recibir/src/main/java/mx/fmre/rttycontest/recibir/services/impl/FileManagerServiceAwsS3Impl.java
@@ -12,13 +12,16 @@ import mx.fmre.rttycontest.persistence.model.Edition;
 import mx.fmre.rttycontest.persistence.model.Email;
 import mx.fmre.rttycontest.persistence.repository.IEditionRepository;
 import mx.fmre.rttycontest.recibir.dto.AttachedFileDTO;
+import mx.fmre.rttycontest.recibir.helper.EditionStaticHelper;
 import mx.fmre.rttycontest.recibir.helper.FileUtil;
 import mx.fmre.rttycontest.recibir.services.AWSS3Service;
 import mx.fmre.rttycontest.recibir.services.IFileManagerService;
 
 @Slf4j
 @Service
-public class FileManagerServiceImpl implements IFileManagerService {
+public class FileManagerServiceAwsS3Impl implements IFileManagerService {
+	
+//	SampleInterface sample = appContext.getBean(service, SampleInterface.class);
 	
 	@Autowired
 	private AWSS3Service s3Service;
@@ -33,11 +36,7 @@ public class FileManagerServiceImpl implements IFileManagerService {
 	public String saveFile(Email email, AttachedFileDTO fileDTO) {
 		log.info("start saving file {}", fileDTO.getFilename());
 		Edition edition = editionRepository.findById(email.getEdition().getId()).orElse(null);
-		String filename = String.format("contest_%d/edition_%d/emailcount_%d/%s", 
-				edition.getContest().getId(),
-				edition.getId(),
-				email.getEmailCount(),
-				fileDTO.getFilename());
+		String filename = EditionStaticHelper.filename(email, edition, fileDTO);
 		ObjectMetadata metaData = new ObjectMetadata();
 		metaData.setContentType(fileDTO.getContenyType());
 		metaData.setContentLength(fileDTO.getByteArray().length);
