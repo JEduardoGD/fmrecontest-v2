484edc19885573841f7247ae24ab4140a1863f58
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2019-11-08 08:32:57 -0600

    test

---
+++ fmrecontest-recibir/src/test/java/mx/fmre/rttycontest/recibir/test/Xxxx.java
@@ -0,0 +1,15 @@
+package mx.fmre.rttycontest.recibir.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xxxx {
+
+	@Test
+	void test() {
+		Assertions.assertEquals(201, 201);
+	}
+
+}
