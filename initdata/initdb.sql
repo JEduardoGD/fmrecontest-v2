274f6502495c93bf63e51dd472e9cf4e24f7edf7
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2019-11-22 08:55:33 -0600

    adding extra rights for fmrecontest user

--- initdata/initdb.sql
+++ initdata/initdb.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE fmrecontest CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER 'fmrecontest'@'%' IDENTIFIED BY 'fmrecontest';
 GRANT ALL PRIVILEGES ON fmrecontest.* TO 'fmrecontest'@'%';
+GRANT SELECT ON performance_schema.user_variables_by_thread TO 'fmrecontest'@'%';
+
