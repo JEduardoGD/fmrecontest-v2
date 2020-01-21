288fddf1032f6ff897be6225359c2d8c41e4f8af
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-01-21 11:07:53 -0600

    Adding attachedFileParser

---
+++ fmrecontest-api/src/main/java/mx/fmre/rttycontest/api/util/AttachedFileUtil.java
@@ -0,0 +1,25 @@
+package mx.fmre.rttycontest.api.util;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import mx.fmre.rttycontest.api.dto.AttachedFileDTO;
+import mx.fmre.rttycontest.persistence.model.AttachedFile;
+
+public class AttachedFileUtil {
+	public List<AttachedFileDTO> map(List<AttachedFile> attachedFiles) {
+		return attachedFiles.stream().map(af -> {
+			return this.map(af);
+		}).collect(Collectors.toList());
+	}
+
+	public AttachedFileDTO map(AttachedFile attachedFile) {
+		AttachedFileDTO a = new AttachedFileDTO();
+		a.setId(attachedFile.getId());
+		a.setFilename(attachedFile.getFilename());
+		a.setContentType(attachedFile.getContentType());
+		a.setLenght(attachedFile.getLenght());
+		a.setPath(attachedFile.getPath());
+		return a;
+	}
+}
