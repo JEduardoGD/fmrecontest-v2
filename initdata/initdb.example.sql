08b705d7eb8fc49cc45129c37f3295e6598dd889
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-01-30 17:22:23 -0600

    fixing first files

--- initdata/initdb.example.sql
+++ initdata/initdb.example.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE fmrecontest CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'fmrecontest'@'%' IDENTIFIED BY 'fmrecontest';
+CREATE USER 'fmrecontest'@'%' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON fmrecontest.* TO 'fmrecontest'@'%';
 GRANT SELECT ON performance_schema.user_variables_by_thread TO 'fmrecontest'@'%';
