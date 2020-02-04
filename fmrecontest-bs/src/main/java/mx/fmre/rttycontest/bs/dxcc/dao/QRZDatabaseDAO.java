c61aef38c14a99b1d358aaba790bc59349ae8ef3
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-02-04 17:21:29 -0600

    Creating QRZ Daos

---
+++ fmrecontest-bs/src/main/java/mx/fmre/rttycontest/bs/dxcc/dao/QRZDatabaseDAO.java
@@ -0,0 +1,35 @@
+package mx.fmre.rttycontest.bs.dxcc.dao;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+
+@XmlRootElement(name = "QRZDatabase")
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+public class QRZDatabaseDAO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -130057701480613577L;
+
+	@XmlAttribute
+	private BigDecimal version;
+
+	@XmlElement(name = "Callsign")
+	private CallsignDAO callsign;
+
+	@XmlElement(name = "DXCC")
+	private DxccentityModelDAO dxcc;
+
+	@XmlElement(name = "Session")
+	private QrzSessionDAO session;
+}
