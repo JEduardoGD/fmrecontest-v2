7826978eda6bbfdcd10aaa41fde29a4f746af23b
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-02-16 23:02:22 -0600

    avance

--- fmrecontest-bs/src/main/java/mx/fmre/rttycontest/bs/reports/service/impl/CsvReportsServiceImpl.java
+++ fmrecontest-bs/src/main/java/mx/fmre/rttycontest/bs/reports/service/impl/CsvReportsServiceImpl.java
@@ -7,12 +7,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,17 +26,22 @@ import org.springframework.stereotype.Service;
 import com.opencsv.CSVWriter;
 
 import mx.fmre.rttycontest.bs.reports.service.ICsvReportsService;
+import mx.fmre.rttycontest.bs.util.csv.CsvUtil;
+import mx.fmre.rttycontest.persistence.model.CatEmailError;
 import mx.fmre.rttycontest.persistence.model.Conteo;
 import mx.fmre.rttycontest.persistence.model.ContestLog;
 import mx.fmre.rttycontest.persistence.model.ContestQso;
 import mx.fmre.rttycontest.persistence.model.Edition;
 import mx.fmre.rttycontest.persistence.model.Email;
+import mx.fmre.rttycontest.persistence.model.EmailStatus;
 import mx.fmre.rttycontest.persistence.model.RelConteoContestLog;
 import mx.fmre.rttycontest.persistence.model.RelQsoConteo;
+import mx.fmre.rttycontest.persistence.repository.ICatEmailErrorRepository;
 import mx.fmre.rttycontest.persistence.repository.IConteoRepository;
 import mx.fmre.rttycontest.persistence.repository.IContestLogRepository;
 import mx.fmre.rttycontest.persistence.repository.IContestQsoRepository;
 import mx.fmre.rttycontest.persistence.repository.IEditionRepository;
+import mx.fmre.rttycontest.persistence.repository.IEmailEstatusRepository;
 import mx.fmre.rttycontest.persistence.repository.IEmailRepository;
 import mx.fmre.rttycontest.persistence.repository.IRelConteoContestLogRepository;
 
@@ -43,58 +54,46 @@ public class CsvReportsServiceImpl implements ICsvReportsService {
 	@Autowired private IContestLogRepository contestLogRepository;
 	@Autowired private IContestQsoRepository contestQsoRepository;
 	@Autowired private IEmailRepository emailRepository;
+	@Autowired private IEmailEstatusRepository emailEstatusRepository;
+	@Autowired private ICatEmailErrorRepository catEmailErrorRepository;
+	
+	private Map<Integer, String> emailStatusesArray;
+	private Map<Integer, String> mapEmmailError;
+	
+	@PostConstruct
+	private void initData() {
+		this.emailStatusesArray = emailEstatusRepository
+				.findAll()
+				.stream()
+				.collect(Collectors.toMap(EmailStatus::getId, EmailStatus::getStatus));
+		
+		this.mapEmmailError = catEmailErrorRepository
+				.findAll()
+				.stream()
+				.collect(Collectors.toMap(CatEmailError::getId, CatEmailError::getDescripcion));
+	}
 	
 	@Override
 	public byte[] getAllEmailsOnByEditionId(int editionId) {
+		
 		Edition edition = editionRepository.findById(editionId).orElse(null);
 		List<Email> emails = emailRepository.findByEdition(edition);
+		
+		
 		String[] header = { 
 				"ID",
 				"SUBJECT",
 				"FROM NAME",
 				"FROM ADDRESS",
-				"SENT DATE",
-				"VERIFIED AT",
-				"ANSWERED AD"};
-		List<String[]> listStringsContent = emails.stream().map(email -> {
-			String[] content = {
-					email.getId()+ "",
-					email.getSubject(),
-					email.getRecipientsFromName(),
-					email.getRecipientsFromAddress(),
-					email.getSentDate().toString(),
-					email.getVerifiedAt().toString(),
-					email.getAnsweredAt().toString()};
-			return content;
-		}).collect(Collectors.toList());
-
-
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		CSVWriter writer = null;
-		try {
-			OutputStreamWriter streamWriter = new OutputStreamWriter(out);
-			// create FileWriter object with file as parameter
-
-			// create CSVWriter object filewriter object as parameter
-			writer = new CSVWriter(streamWriter, ';', 
-					CSVWriter.NO_QUOTE_CHARACTER, 
-					CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-					CSVWriter.DEFAULT_LINE_END);
-
-			writer.writeNext(header);
-
-			for (String[] s : listStringsContent) {
-				writer.writeNext(s);
-			}
-		} finally {
-			if (writer != null)
-				try {
-					writer.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		}
-		return out.toByteArray();
+				"SENT AT UTC",
+				"VERIFIED AT UTC",
+				"ANSWERED AT UTC", 
+				"STATUS",
+				"ERRORS"};
+		
+		List<String[]> listStringsContent = CsvUtil.listEmailsToStrings(emails, catEmailErrorRepository, mapEmmailError, emailStatusesArray);
+		
+		return CsvUtil.createCsvByteArray(header, listStringsContent);
 	}
 
 	@Override
--- fmrecontest-bs/src/main/java/mx/fmre/rttycontest/bs/util/csv/CsvUtil.java
+++ fmrecontest-bs/src/main/java/mx/fmre/rttycontest/bs/util/csv/CsvUtil.java
@@ -1,11 +1,66 @@
 package mx.fmre.rttycontest.bs.util.csv;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import mx.fmre.rttycontest.persistence.model.RelConteoContestLog;
+import com.opencsv.CSVWriter;
+
+import mx.fmre.rttycontest.persistence.model.CatEmailError;
+import mx.fmre.rttycontest.persistence.model.Email;
+import mx.fmre.rttycontest.persistence.repository.ICatEmailErrorRepository;
 
 public class CsvUtil {
-	public List<String[]> getStringArray(List<RelConteoContestLog> relConteoContestLogs) {
-		return null;
+	
+	private final static DateFormat df = new SimpleDateFormat("yy-MM-dd HH:mm:ss", Locale.ENGLISH);
+	
+	public static List<String[]> listEmailsToStrings(
+			List<Email> emails,
+			ICatEmailErrorRepository catEmailErrorRepository,
+			Map<Integer, String> mapEmmailError,
+			Map<Integer, String> emailStatusesArray) {
+		List<String[]> listStringsContent = emails.stream().map(email -> {
+			List<CatEmailError> emailErrors = catEmailErrorRepository.getErrorsOfEmail(email);
+			List<String> errors = emailErrors.stream().map(x -> mapEmmailError.get(x.getId())).collect(Collectors.toList());
+			String[] content = {
+					email.getId() + "",
+					email.getSubject(),
+					email.getRecipientsFromName(),
+					email.getRecipientsFromAddress(),
+					df.format(email.getSentDate()),
+					df.format(email.getVerifiedAt()),
+					df.format(email.getAnsweredAt()),
+					emailStatusesArray.get(email.getEmailStatus().getId()),
+					String.join(";", errors)};
+			return content;
+		}).collect(Collectors.toList());
+		return listStringsContent;
+	}
+	
+	public static byte[] createCsvByteArray(String[] header, List<String[]> listStringsContent) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		CSVWriter writer = null;
+		try {
+			OutputStreamWriter streamWriter = new OutputStreamWriter(out);
+			writer = new CSVWriter(streamWriter);
+			writer.writeNext(header);
+			for (String[] s : listStringsContent) {
+				writer.writeNext(s);
+			}
+		} finally {
+			if (writer != null)
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		return out.toByteArray();
 	}
 }
