7ce0719e054d132559fee10054222740b9613a48
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-02-25 07:22:17 -0600

    avance

--- fmrecontest-persistence/src/main/java/mx/fmre/rttycontest/persistence/repository/IVBandFrequencyRepository.java
+++ fmrecontest-persistence/src/main/java/mx/fmre/rttycontest/persistence/repository/IVBandFrequencyRepository.java
@@ -1,5 +1,19 @@
 package mx.fmre.rttycontest.persistence.repository;
 
-public class IVBandFrequencyRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import mx.fmre.rttycontest.persistence.model.VBandFrequency;
+
+@Repository
+public interface IVBandFrequencyRepository extends JpaRepository<VBandFrequency, Integer> {
+	@Query(value = "" +
+			"select v "	+ 
+			"from VBandFrequency v " + 
+			"where v.startFrequency <= :frequency and " + 
+			"            :frequency <= v.endFrequency ")
+	public List<VBandFrequency> findByFrequency(int frequency);
 }
