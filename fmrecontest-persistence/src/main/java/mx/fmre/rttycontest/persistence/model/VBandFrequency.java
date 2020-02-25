12e7a37e1795e80964d35b0a57f511f5f43d8297
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-02-24 18:54:32 -0600

    avance

---
+++ fmrecontest-persistence/src/main/java/mx/fmre/rttycontest/persistence/model/VBandFrequency.java
@@ -0,0 +1,33 @@
+package mx.fmre.rttycontest.persistence.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TBL_USERS")
+public class VBandFrequency {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "N_ID_BAND")
+	private Integer id;
+	
+	@Column(name = "S_BAND")
+	private String band;
+	
+	@Column(name = "MIN")
+	private BigDecimal startFrequency;
+	
+	@Column(name = "MAX")
+	private BigDecimal endFrequency;
+}
---
+++ fmrecontest-persistence/src/main/java/mx/fmre/rttycontest/persistence/repository/IVBandFrequencyRepository.java
@@ -0,0 +1,5 @@
+package mx.fmre.rttycontest.persistence.repository;
+
+public class IVBandFrequencyRepository {
+
+}
