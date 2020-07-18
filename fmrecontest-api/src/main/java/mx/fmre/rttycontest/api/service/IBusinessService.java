6f68abce25cc21b340e762efe793a9bc918b7765
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-07-18 09:22:20 -0500

    fix some errors

--- fmrecontest-api/src/main/java/mx/fmre/rttycontest/api/service/IBusinessService.java
+++
@@ -1,8 +0,0 @@
-package mx.fmre.rttycontest.api.service;
-
-import mx.fmre.rttycontest.api.dto.ContestlogDto;
-import mx.fmre.rttycontest.exception.FmreContestException;
-
-public interface IBusinessService {
-	public ContestlogDto recalculateLog(ContestlogDto contestlogDto) throws FmreContestException;
-}
