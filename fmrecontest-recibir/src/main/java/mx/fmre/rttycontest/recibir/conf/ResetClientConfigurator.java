76ea6d6e29ed41fe4e45f7bc383c311d7d952c83
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2025-02-03 17:34:09 -0600

    fix enconding

--- fmrecontest-recibir/src/main/java/mx/fmre/rttycontest/recibir/conf/ResetClientConfigurator.java
+++ fmrecontest-recibir/src/main/java/mx/fmre/rttycontest/recibir/conf/ResetClientConfigurator.java
@@ -1,7 +1,10 @@
 package mx.fmre.rttycontest.recibir.conf;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -15,7 +18,9 @@ public class ResetClientConfigurator {
 	public RestTemplate restTemplate() {
 		log.info("create rest template");
 		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
 		restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://192.168.0.199:9080"));
+		//restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:9080"));
 		return restTemplate;
 	}
 }
