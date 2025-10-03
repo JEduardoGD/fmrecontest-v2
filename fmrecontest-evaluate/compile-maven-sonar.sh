fa0b14311caa55d32e9895ae6def7bfda02c98c0
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2025-10-03 07:41:35 -0600

    adding qrz env var

---
+++ fmrecontest-evaluate/compile-maven-sonar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MAVEN_HOME=/home/geduardo/sft/maven/apache-maven-3.6.1
+JAVA_HOME=/home/geduardo/sft/java/jdk1.8.0_221
+
+export PATH=$PATH:$MAVEN_HOME/bin:$JAVA_HOME/bin
+
+mvn -v
+
+mvn clean package -Dmaven.test.skip=true
+
+mvn sonar:sonar
+
